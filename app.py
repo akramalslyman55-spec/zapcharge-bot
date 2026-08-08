f6837c2200544fe32c03f69a99d1ed99ff76c462
akramalslyman55-spec <akramalslyman55@gmail.com>
2026-08-08 22:01:37 +0300

    Update app.py

--- app.py
+++ app.py
@@ -766,4 +766,4 @@ def broadcast_message():
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=False)
